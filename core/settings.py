ad13c4120f9bd70791a14eaf48ecad2fdfc3416f
sZ <sazz.sharma42@gmail.com>
2024-04-08 01:19:55 +0545

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -172,3 +172,6 @@ DJOSER = {
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
