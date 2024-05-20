b2442aff35accb3d40d71c8f13bf39d1c301007b
Sazz Sharma <sazz.sharma42@gmail.com>
2024-05-20 20:18:58 +0545

    Disabled Browsable API

--- core/settings.py
+++ core/settings.py
@@ -147,6 +147,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    "DEFAULT_RENDERER_CLASSES": (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
